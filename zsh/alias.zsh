b893d082bf800c750dd78b86be3c8d6401012830
alan910127 <alan469357@gmail.com>
2023-06-15 21:42:33 +0800

    feat: add more aliases

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,10 +1,15 @@
 # command line utilities
+alias l="exa -lah"
+alias ll="exa -lh"
 alias ls=exa
 
 # git aliases
 alias gp="git push"
 alias gl="git remote prune && git pull --ff-only"
 alias gco="git checkout"
+alias gst="git status"
+alias gcl="git clone"
+
 
 # tools
 function take() {

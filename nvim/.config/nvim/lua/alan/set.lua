8da4230a6beb0355887a45c74272d67fff085446
alan910127 <70696274+alan910127@users.noreply.github.com>
2024-03-09 23:31:12 +0800

    feat: neovim

---
+++ nvim/.config/nvim/lua/alan/set.lua
@@ -0,0 +1,62 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Show relative line numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- Enable mouse mode
+vim.opt.mouse = "a"
+
+-- Disable showing current mode, since it's in statusline
+vim.opt.showmode = false
+
+-- Enable break indent
+--   Wrapped lines will show the same amount of indentation.
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace in the editor.
+--   See :help 'list'
+--   and :help 'listchars'
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions in real time
+vim.opt.inccommand = "split"
+
+-- Show current line the cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
+
+-- Set highlight on search, but clear on pressing <Esc> in normal mode.
+--   It's more akin to a setting, so the remap is placed here :(
+vim.opt.hlsearch = true
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking text",
+  group = vim.api.nvim_create_augroup("YankHighlight", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})

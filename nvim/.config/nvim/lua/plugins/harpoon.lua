5b1f652328cadf924edd1ecb2413a46c811ce784
alan910127 <70696274+alan910127@users.noreply.github.com>
2024-03-28 23:29:19 +0800

    feat: harpoon lets go

---
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,33 @@
+return {
+  "ThePrimeagen/harpoon",
+  branch = "harpoon2",
+  config = function()
+    local harpoon = require("harpoon")
+    harpoon:setup({
+      settings = {
+        save_on_toggle = true,
+      },
+    })
+
+    ---@param key string
+    ---@param cmd fun():nil
+    ---@param desc string
+    local map = function(key, cmd, desc)
+      vim.keymap.set("n", key, cmd, { desc = "Harpoon: " .. desc })
+    end
+
+    map("<leader>a", function()
+      harpoon:list():append()
+    end, "[A]ppend")
+    map("<C-e>", function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end, "Quick [E]dit")
+
+    map("<C-P>", function()
+      harpoon:list():prev()
+    end, "[P]revious Buffer")
+    map("<C-N>", function()
+      harpoon:list():next()
+    end, "[N]ext Buffer")
+  end,
+}

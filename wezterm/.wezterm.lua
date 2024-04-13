73b19762eb8fd02b0bc8f2e6d4894b70e11eb4e9
alan910127 <70696274+alan910127@users.noreply.github.com>
2024-04-13 17:30:49 +0800

    chore: lua files consistent formatting

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,31 +1,31 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local mux = wezterm.mux
 
 -- Maximize window on startup
-wezterm.on('gui-startup', function()
-  local _, _, window = mux.spawn_window {}
+wezterm.on("gui-startup", function()
+  local _, _, window = mux.spawn_window({})
   window:gui_window():maximize()
 end)
 
 return {
   -- Font
-  font = wezterm.font 'JetBrains Mono',
+  font = wezterm.font("JetBrains Mono"),
   -- Tab Bar
   hide_tab_bar_if_only_one_tab = true,
   -- Launch WSL by default
-  default_prog = { 'wsl', '--cd', '~' },
+  default_prog = { "wsl", "--cd", "~" },
   -- Disable Close Window Confirmation Menu
-  window_close_confirmation = 'NeverPrompt',
+  window_close_confirmation = "NeverPrompt",
 
   keys = {
     {
-      mods = 'CTRL|SHIFT',
-      key = 'n',
+      mods = "CTRL|SHIFT",
+      key = "n",
       action = wezterm.action.DisableDefaultAssignment,
     },
     {
-      mods = 'CTRL|SHIFT',
-      key = 'p',
+      mods = "CTRL|SHIFT",
+      key = "p",
       action = wezterm.action.DisableDefaultAssignment,
     },
   },

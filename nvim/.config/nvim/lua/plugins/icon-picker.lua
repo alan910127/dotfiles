50498fb42a86f770a592db3d045127f898dd5598
alan910127 <70696274+alan910127@users.noreply.github.com>
2024-05-16 08:49:44 +0800

    feat: neovim icon picker

---
+++ nvim/.config/nvim/lua/plugins/icon-picker.lua
@@ -0,0 +1,18 @@
+return {
+  "ziontee113/icon-picker.nvim",
+  config = function()
+    require("icon-picker").setup({ disable_legacy_commands = true })
+
+    ---@param mode string
+    ---@param key string
+    ---@param func string|fun():nil
+    ---@param desc string?
+    local map = function(mode, key, func, desc)
+      vim.keymap.set(mode, key, func, { noremap = true, silent = true, desc = desc })
+    end
+
+    map("n", "<leader><leader>i", "<CMD>IconPickerNormal<CR>", "[I]con Picker")
+    map("n", "<leader><leader>y", "<CMD>IconPickerYank<CR>", "Icon Picker [Y]ank Selected")
+    map("n", "<C-i>", "<CMD>IconPickerInsert<CR>", "[I]con Picker")
+  end,
+}

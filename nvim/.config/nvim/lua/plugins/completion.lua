9c84711c3bf709a4a25a5e4049cd925c35a44110
alan910127 <70696274+alan910127@users.noreply.github.com>
2024-05-22 00:15:11 +0800

    ui: rounded completion menu

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -34,6 +34,10 @@ return {
         },
         completion = { completeopt = "menu,menuone,noinsert" },
 
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
         formatting = {
           format = lspkind.cmp_format({
             mode = "symbol_text",

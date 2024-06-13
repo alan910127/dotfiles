6079f4d68c2efabde5b97c310e3671ab1a928439
alan910127 <70696274+alan910127@users.noreply.github.com>
2024-06-13 15:16:04 +0800

    feat: update oil.nvim settings

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,3 +1,8 @@
+local HIDDEN_NAMES = {
+  "..",
+  ".git",
+}
+
 return {
   "stevearc/oil.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -5,8 +10,13 @@ return {
     local oil = require("oil")
 
     oil.setup({
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
       view_options = {
         show_hidden = true,
+        is_always_hidden = function(name, bufnr)
+          return vim.list_contains(HIDDEN_NAMES, name)
+        end,
       },
     })
 

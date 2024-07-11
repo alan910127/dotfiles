a88e799bae53ab7db7dc4fa8b10195606161e5a0
alan910127 <70696274+alan910127@users.noreply.github.com>
2024-07-11 23:56:37 +0800

    chore: update plugins

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -15,9 +15,8 @@ return {
     "stevearc/conform.nvim",
     opts = {
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
+      format_after_save = {
+        lsp_format = "fallback",
       },
       formatters_by_ft = {
         lua = { "stylua" },
--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -1,7 +1,6 @@
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  branch = "dev",
   config = function()
     local trouble = require("trouble")
     trouble.setup()

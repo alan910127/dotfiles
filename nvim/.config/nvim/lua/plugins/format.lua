01b41c41730beb97031847a8acd5b26ebb185770
alan910127 <70696274+alan910127@users.noreply.github.com>
2024-08-10 09:44:33 +0800

    chore: update neovim plugins w/ configs specs

--- nvim/.config/nvim/lua/plugins/format.lua
+++ nvim/.config/nvim/lua/plugins/format.lua
@@ -1,9 +1,15 @@
-local function get_js_formatter()
-  local biome_installed = vim.fn.isdirectory(vim.fn.getcwd() .. "/node_modules/@biomejs/biome")
-  if biome_installed then
-    return { "biome" }
+---@param bufnr integer
+---@param ... string
+---@return string[]
+local function first(bufnr, ...)
+  local conform = require("conform")
+  for i = 1, select("#", ...) do
+    local formatter = select(i, ...)
+    if conform.get_formatter_info(bufnr, formatter).available then
+      return formatter
+    end
   end
-  return { { "prettier", "prettierd" } }
+  return select(1, ...)
 end
 
 return {
@@ -13,27 +19,55 @@ return {
   -- Auto formatting
   {
     "stevearc/conform.nvim",
-    opts = {
-      notify_on_error = false,
-      format_after_save = {
-        lsp_format = "fallback",
-      },
-      formatters_by_ft = {
-        lua = { "stylua" },
-        python = { "ruff_format", "ruff_fix" },
-        javascript = get_js_formatter,
-        javascriptreact = get_js_formatter,
-        typescript = get_js_formatter,
-        typescriptreact = get_js_formatter,
-        astro = { "prettier" },
-        markdown = { "prettier" },
-        sh = { "shfmt" },
-      },
-      formatters = {
-        shfmt = {
-          prepend_args = { "-i", "4" },
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    config = function()
+      local conform = require("conform")
+
+      conform.setup({
+        notify_on_error = false,
+        format_after_save = {
+          lsp_format = "fallback",
         },
-      },
-    },
+        formatters_by_ft = {
+          lua = { "stylua" },
+          python = { "ruff_format", "ruff_fix" },
+          javascript = function(bufnr)
+            return { first(bufnr, "biome", "prettier") }
+          end,
+          javascriptreact = function(bufnr)
+            return { first(bufnr, "biome", "prettier") }
+          end,
+          typescript = function(bufnr)
+            return { first(bufnr, "biome", "prettier") }
+          end,
+          typescriptreact = function(bufnr)
+            return { first(bufnr, "biome", "prettier") }
+          end,
+          astro = function(bufnr)
+            return { first(bufnr, "prettier") }
+          end,
+          markdown = function(bufnr)
+            return { first(bufnr, "prettier") }
+          end,
+          json = function(bufnr)
+            return { first(bufnr, "prettier", "jsonls") }
+          end,
+          jsonc = function(bufnr)
+            return { first(bufnr, "prettier", "jsonls") }
+          end,
+          sh = { "shfmt" },
+        },
+        formatters = {
+          shfmt = {
+            prepend_args = { "-i", "4" },
+          },
+        },
+      })
+
+      vim.keymap.set("n", "<leader>f", function()
+        conform.format({ async = true })
+      end, { desc = "[F]ormat Buffer" })
+    end,
   },
 }
--- nvim/.config/nvim/lua/plugins/quality_of_life.lua
+++ nvim/.config/nvim/lua/plugins/quality_of_life.lua
@@ -6,14 +6,15 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     config = function()
-      require("which-key").setup()
+      local wk = require("which-key")
+      wk.setup()
 
-      require("which-key").register({
-        ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-        ["<leader>d"] = { name = "[D]coument", _ = "which_key_ignore" },
-        ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-        ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-        ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+      wk.add({
+        { "<leader>c", group = "[C]ode" },
+        { "<leader>d", group = "[D]coument" },
+        { "<leader>r", group = "[R]ename" },
+        { "<leader>s", group = "[S]earch" },
+        { "<leader>w", group = "[W]orkspace" },
       })
     end,
   },
